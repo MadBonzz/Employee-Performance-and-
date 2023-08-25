b173bb1f2dd59b8a4e4b2bca38ab74f71aa013ee
Shourya Jain <shourya242@gmail.com>
2023-08-25 21:03:53 +0530

    logging, errors, utils files

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str)->List[str]:
     #returns list of libraries
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replce("\n", " ") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
 
 setup(
     name= 'Performance and Attriton',
